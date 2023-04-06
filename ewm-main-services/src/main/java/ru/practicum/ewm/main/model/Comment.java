36d32873d01a40dea86636a528b6e9c2ed01c015
Bikkul <quadalus@yandex.ru>
2023-04-06 15:54:09 +0300

    feat: add comment model

---
+++ ewm-main-services/src/main/java/ru/practicum/ewm/main/model/Comment.java
@@ -0,0 +1,47 @@
+package ru.practicum.ewm.main.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "comments")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "comment_text", nullable = false, length = 500)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "comment_user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "comment_event_id")
+    private Event event;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Comment comment = (Comment) o;
+        return getId() != null && Objects.equals(getId(), comment.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
